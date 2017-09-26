251f8d9f4e45c0d9f02df354fd6a26baa26b0edc
Alex <alexander.martin@ccbc.edu>
2017-09-25 20:21:37 -0400

    - Add Click Event

--- src/MakeSquare.java
+++ src/MakeSquare.java
@@ -1,6 +1,9 @@
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+
+import java.util.Random;
 
 public class MakeSquare implements Shapes {
     private Rectangle rect;
@@ -25,7 +28,16 @@ public class MakeSquare implements Shapes {
         rect = new Rectangle(x, y, color);
         rect.setTranslateX(locationX);
         rect.setTranslateY(locationY);
+        rect.setOnMousePressed(this::actiononpress);
     }
+
+    private void actiononpress(MouseEvent mouseEvent) {
+        Random rand = new Random();
+        rect.setTranslateX(rand.nextInt(1200));
+        rect.setTranslateY(rand.nextInt(680));
+        System.out.println("Pressed");
+    }
+
 
     @Override
     public int getX() {
